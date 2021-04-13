7e991310edcc78fd96302546919f7fc23b2f1033
infsolution <clinfsolution@gmail.com>
2021-04-13 12:10:20 -0300

    correction bug invited Controller

--- start/routes/invited.js
+++ start/routes/invited.js
@@ -3,5 +3,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 Route.group(()=>{
-  Route.get('party/:id/:invited', 'InvitCon:troller.invite').as('invited')
+  Route.get('party/:id/:invited', 'InvitController.invite').as('invited')
 }).prefix('v1/').namespace('Invited')
